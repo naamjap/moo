313e05400e19a681dc996f76510f02505de14647
naamjap <naam_jap@hotmail.com>
2025-04-20 19:32:46 -0400

    updates

--- mnmoo/src/pushrepo.sh
+++
@@ -1,7 +0,0 @@
-echo "# moo" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/naamjap/moo.git
-git push -u origin main
